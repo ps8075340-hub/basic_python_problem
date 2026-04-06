ed44c5da79f27e8699de273b5462901f7cb85de7
ps8075340-hub <ps8075340@gmail.com>
2026-04-06 17:18:30 +0530

    Create simple_generate_divisors_number.py

---
+++ simple_generate_divisors_number.py
@@ -0,0 +1,9 @@
+# Program to generate divisors of a number.
+
+num = int(input("Enter a number: "))
+
+print("Divisors of", num, "are:")
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        print(i)
