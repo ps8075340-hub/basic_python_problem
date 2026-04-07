4c196f9cdbd87ad879f457191b819293dbb90220
ps8075340-hub <ps8075340@gmail.com>
2026-04-07 17:20:49 +0530

    Create simple_fizzbuzz_problem.py

---
+++ simple_fizzbuzz_problem.py
@@ -0,0 +1,14 @@
+# FizzBuzz problem
+
+n = int (input ('Enter a number : '))
+
+# Check
+for i in range(1, n + 1):
+   if i % 3 == 0 and 5 == 0:
+       print(i, '- FizzBuzz')
+   elif i % 3 == 0:
+       print(i, '- Fizz')
+   elif i % 5 == 0:
+       print(i, '- Buzz')
+   else:
+       print(i, '- Not Divisible by 3 and 5!') 
