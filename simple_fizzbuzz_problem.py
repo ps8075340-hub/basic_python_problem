d89db900ce7351486e1559024a4083b04d4f25b9
ps8075340-hub <ps8075340@gmail.com>
2026-04-28 16:03:27 +0530

    Update simple_fizzbuzz_problem.py

--- simple_fizzbuzz_problem.py
+++ simple_fizzbuzz_problem.py
@@ -4,7 +4,7 @@ n = int (input ('Enter a number : '))
 
 # Check
 for i in range(1, n + 1):
-   if i % 3 == 0 and 5 == 0:
+   if i % 3 == 0 and i % 5 == 0:
        print(i, '- FizzBuzz')
    elif i % 3 == 0:
        print(i, '- Fizz')
