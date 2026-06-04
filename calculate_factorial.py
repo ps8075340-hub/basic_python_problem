0651e9fbfae1e6727236990de3171a358e8c13bd
ps8075340-hub <ps8075340@gmail.com>
2026-06-04 11:22:36 +0530

    Create calculate_factorial.py

---
+++ calculate_factorial.py
@@ -0,0 +1,10 @@
+# Function to calculate factorial of a number. 
+def factorial(n):
+    result = 1
+
+    for i in range(1, n + 1):
+        result = result * i
+
+    return result
+
+print(factorial(int(input("Enter a number : "))))
